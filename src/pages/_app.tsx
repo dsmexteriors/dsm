47aa87b670c21066bff29e15d35dc13a3f9dc06b
JD Fillmore <jd@infused.agency>
2023-06-07 22:27:12 -0400

    add bs css above all other styles to make css work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import "@/styles/globals.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "../styles/general.scss";
 import "../styles/typography.scss";
 import "../styles/button.scss";
@@ -6,8 +6,6 @@ import type { AppProps } from "next/app";
 import { Poppins } from "next/font/google";
 import Navigation from "../../components/shared/nav/Navigation";
 import Footer from "../../components/shared/footer/Footer";
-
-import "bootstrap/dist/css/bootstrap.min.css";
 
 const poppins = Poppins({
   weight: ["400", "600"],
