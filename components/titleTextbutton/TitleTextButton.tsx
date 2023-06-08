e823920a91b294aad05277ee98b714b419f9348b
JD Fillmore <jd@infused.agency>
2023-06-08 17:48:31 -0400

    Working on home portfolio

--- components/titleTextbutton/TitleTextButton.tsx
+++ components/titleTextbutton/TitleTextButton.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import styles from "../../src/styles/home-portfolio.module.scss";
+
 interface TitleTextButtonProps {
   title: string;
   text: string;
@@ -13,7 +15,7 @@ const TitleTextButton: React.FC<TitleTextButtonProps> = ({ title, text, btnLink,
       <h2>{title}</h2>
       <hr />
       <p>{text}</p>
-      <Link className="btn btn-primary" href={btnLink}>
+      <Link className={`btn btn-primary ${styles["btn"]}`} href={btnLink}>
         {btnText}
       </Link>
     </>
