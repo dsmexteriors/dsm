afec5b2e46829bb370158856683313a30c1f2d87
JD Fillmore <jd@infused.agency>
2023-06-12 15:56:05 -0400

    base pages created

---
+++ src/pages/contact.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
+import { Container, Row, Col } from "react-bootstrap";
+import { InnerHero } from "../../components/inner-hero/InnerHero";
+
+const Contact = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  const mainControls = useAnimation();
+
+  useEffect(() => {
+    if (isInView) {
+      mainControls.start("visible");
+    }
+  }, [isInView]);
+  return (
+    <>
+      <div ref={ref}>
+        <InnerHero title="Contact Us" />
+        <Container>
+          <Row>
+            <Col lg="12">
+              <motion.div
+                variants={{ hidden: { opacity: 0, y: 35 }, visible: { opacity: 1, y: 0 } }}
+                initial="hidden"
+                animate={mainControls}
+                transition={{ duration: 0.5, delay: 0.2 }}
+              >
+                <p>Content</p>
+              </motion.div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
